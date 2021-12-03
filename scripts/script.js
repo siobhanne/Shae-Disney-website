4c4237b8bea6903d7d23a7b929db95f8f03d17bf
siobhanne <siobhannedrost@hotmail.com>
2021-12-03 14:34:37 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// // JavaScript Document
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu(event) {
+  // deNav = event.target.parentNode;
+  // deNav.classList.toggle("toonMenu");
+
+  document.body.classList.toggle("toonMenu");
+}
+
+
+
+var deSearch = document.querySelector("nav label");
+
+deSearch.addEventListener("click", toggleSearch);
+
+function toggleSearch(event) {
+  searchBalk = event.target.parentNode;
+  searchBalk.classList.toggle("toonSearch");
+}
