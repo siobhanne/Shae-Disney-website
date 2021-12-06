a1bb2d28a71ff6b161564ca9d9ef98bb972ccc40
siobhanne <siobhannedrost@hotmail.com>
2021-12-06 22:17:03 +0100

    dark/light mode; section 4: articles; footer update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 // // JavaScript Document
+
+// BURGERMENU
+
 var deButton = document.querySelector("nav button");
 
 deButton.addEventListener("click", toggleMenu);
@@ -12,6 +15,9 @@ function toggleMenu(event) {
 
 
 
+
+// SEARCH INPUT
+
 var deSearch = document.querySelector("nav label");
 
 deSearch.addEventListener("click", toggleSearch);
@@ -19,4 +25,33 @@ deSearch.addEventListener("click", toggleSearch);
 function toggleSearch(event) {
   searchBalk = event.target.parentNode;
   searchBalk.classList.toggle("toonSearch");
+}
+
+
+
+// DARK / LIGHT MODE
+var logoChange = document.querySelector("header nav > a");
+
+var lichteKnop = document.querySelector("header nav ul li:nth-of-type(9) button");
+
+lichteKnop.addEventListener("click", wordtLicht);
+
+function wordtLicht(event) {
+  document.documentElement.classList.remove("colorModeChange");
+  document.body.classList.remove("toonMenu");
+
+  document.getElementById('logo').src='../images/disneyLogo.svg';
+}
+
+
+
+var donkereKnop = document.querySelector("header nav ul li:last-of-type button");
+
+donkereKnop.addEventListener("click", wordtDonker);
+
+function wordtDonker(event) {
+  document.documentElement.classList.add("colorModeChange");
+  document.body.classList.remove("toonMenu");
+
+  document.getElementById('logo').src='../images/disneylandLogoWhite.png';
 }
