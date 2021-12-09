6f4e3659529eb30bcce150fcbacba52e16889c0e
siobhanne <siobhannedrost@hotmail.com>
2021-12-09 22:29:21 +0100

    tweede page is af

--- scripts/script.js
+++ scripts/script.js
@@ -31,8 +31,6 @@ function toggleSearch(event) {
 
 
 // DARK / LIGHT MODE
-var logoChange = document.querySelector("header nav > a");
-
 var lichteKnop = document.querySelector("header nav ul li:nth-of-type(9) button");
 
 lichteKnop.addEventListener("click", wordtLicht);
@@ -40,8 +38,6 @@ lichteKnop.addEventListener("click", wordtLicht);
 function wordtLicht(event) {
   document.documentElement.classList.remove("colorModeChange");
   document.body.classList.remove("toonMenu");
-
-  // document.getElementById('logo').src='../images/disneyLogo.svg';
 }
 
 
@@ -53,6 +49,28 @@ donkereKnop.addEventListener("click", wordtDonker);
 function wordtDonker(event) {
   document.documentElement.classList.add("colorModeChange");
   document.body.classList.remove("toonMenu");
+}
 
-  // document.getElementById('logo').src='../images/disneylandLogoWhite.png';
+
+
+
+// VERDER LEZEN
+var verderButton = document.querySelector("#verderLezen button");
+var verderTekst = document.querySelector("#verderLezen p");
+
+verderButton.addEventListener("click", leesVerder);
+
+function leesVerder() {
+    document.querySelector("#verderLezen p").innerHTML = "Sommige shows en parades in de parken kunnen worden gewijzigd, vertraagd of geannuleerd zonder dat wij de gasten vooraf hierover kunnen informeren. Dit kan het gevolg zijn van slecht weer, belangrijke werkzaamheden, wijzigingen van openingstijden of sluiting van attracties. Gelukkig zijn er altijd allerlei andere fantastische attracties en activiteiten. Vraag gerust onze Cast Members (Disney medewerkers) om meer informatie!";
+    document.querySelector("#verderLezen button").innerHTML = "Toon minder";  
+    verderButton.addEventListener("click", toonMinder);
+    console.log("verder lezen");  
+}
+
+function toonMinder() {
+  document.querySelector("#verderLezen p").innerHTML = "Sommige shows en parades in de parken kunnen worden gewijzigd, vertraagd of geannuleerd zonder dat wij de gasten vooraf hierover kunnen informeren. ...";
+  document.querySelector("#verderLezen button").innerHTML = "Verder lezen";  
+  console.log("Toon minder");
+
+  verderButton.addEventListener("click", leesVerder);
 }
